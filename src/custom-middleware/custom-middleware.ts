696c4428193efb815fc2fa1f5898312b7c93728f
Sudesh Gama <sudeshgama@Mac.attlocal.net>
2025-01-29 14:37:41 -0600

    Deployment

--- src/custom-middleware/custom-middleware.ts
+++ src/custom-middleware/custom-middleware.ts
@@ -2,11 +2,11 @@ import express from 'express';
 
 const middleware = express();
 
-middleware.use((req, res, next) => {
-  req.shh__secret = 'doggy'
-  res.status(401)
-  res.send('Nope')
-});
+// middleware.use((req, res, next) => {
+//   req.shh__secret = 'doggy'
+//   res.status(401)
+//   res.send('Nope')
+// });
 
 export default middleware;
 
